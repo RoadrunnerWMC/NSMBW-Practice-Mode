2588e1c6ecdb447f82a0f077f70236b129ded459
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2024-11-15 23:02:27 -0500

    Zone reload: save/restore in-game timer

--- code/src/zone_reload.cpp
+++ code/src/zone_reload.cpp
@@ -8,6 +8,14 @@ NextGoto FakeEntrance = {0};
 bool ForceFacingLeft = false;
 
 
+struct ZoneState {
+    u32 timerPreciseTime;
+};
+
+ZoneState initial_zone_state;
+bool is_restoring = false;
+
+
 u8 curEntranceNonFake = 0;
 kmBranchDefAsm(0x809261bc, 0x809261c0) {
     nofralloc
@@ -25,44 +33,58 @@ fake:
 }
 
 
-void trigger_zone_reload(daPlBase_c *player, bool keep_current_pos) {
-    // Reset stored sprite data in dActorCreateMng_c
-    // (resets spawn status for various types of enemies, mainly)
-    // TODO: maybe just reset this for only the current zone, instead of
-    // the whole level?
-    // Since storedShorts and storedBytes are adjacent in memory, for
-    // efficiency, I just clear both of them with one call
-    memset(&dActorCreateMng_c::m_instance->storedShorts, 0, 2000 + 1000);
+// Hook at the end of dScStage_c::create()
+kmBranchDefCpp(0x80924e58, NULL, u32, ) {
+    if (!is_restoring) {
+        initial_zone_state.timerPreciseTime = dStageTimer_c::m_instance->preciseTime;
+    } else {
+        // Reset stored sprite data in dActorCreateMng_c
+        // (resets spawn status for various types of enemies, mainly)
+        // TODO: maybe just reset this for only the current zone,
+        // instead of the whole level?
+        // Since storedShorts and storedBytes are adjacent in memory,
+        // for efficiency, I just clear both of them with one call
+        memset(&dActorCreateMng_c::m_instance->storedShorts, 0, 2000 + 1000);
 
-    // Reset the bitfield arrays in dBgParameter_c
-    // (resets spawn status for various types of coins and blocks)
-    for (int i = 0; i < 12; i++) {
-        memset(dBgParameter_c::ms_Instance_p->tileBuffers[i], 0, 0x10000);
+        // Reset the bitfield arrays in dBgParameter_c
+        // (resets spawn status for various types of coins and blocks)
+        for (int i = 0; i < 12; i++) {
+            memset(dBgParameter_c::ms_Instance_p->tileBuffers[i], 0, 0x10000);
+        }
+        // TODO: this doesn't affect tile-based coins/blocks until the
+        // area is reloaded
+        // TODO: maybe just reset this for only the current zone,
+        // instead of the whole level?
+
+        // Reset dFlagCtrl_c
+        // (resets spawn status for star coins, red rings, roulette
+        // blocks, etc.)
+        dFlagCtrl_c::m_instance->clearAllFlagData();
+        // TODO: maybe just reset this for only the current zone,
+        // instead of the whole level?
+
+        // Reset all star coins to uncollected
+        dScStage_c::mCollectionCoin[0] = 4;
+        dScStage_c::mCollectionCoin[1] = 4;
+        dScStage_c::mCollectionCoin[2] = 4;
+
+        // If a checkpoint is collected, clear it
+        // (TODO: should really reset it to whatever it was at level
+        // load, but it seems calling cyuukan->courseIN() doesn't do
+        // that)
+        dInfo_c::m_instance->cyuukan.clear();
+
+        // Reset the timer time
+        dStageTimer_c::m_instance->preciseTime = initial_zone_state.timerPreciseTime;
+
+        is_restoring = false;
     }
-    // TODO: this doesn't affect tile-based coins/blocks until the area
-    // is reloaded
-    // TODO: maybe just reset this for only the current zone, instead of
-    // the whole level?
 
-    // Reset dFlagCtrl_c
-    // (resets spawn status for star coins, red rings, roulette blocks,
-    // etc.)
-    dFlagCtrl_c::m_instance->clearAllFlagData();
-    // TODO: maybe just reset this for only the current zone, instead of
-    // the whole level?
+    return 1;
+}
 
-    // Reset all star coins to uncollected
-    dScStage_c::mCollectionCoin[0] = 4;
-    dScStage_c::mCollectionCoin[1] = 4;
-    dScStage_c::mCollectionCoin[2] = 4;
 
-    // If a checkpoint is collected, clear it
-    // (TODO: should really reset it to whatever it was at level load,
-    // but it seems calling cyuukan->courseIN() doesn't do that)
-    dInfo_c::m_instance->cyuukan.clear();
-
-    // Finally actually do the warp
-
+void trigger_zone_reload(daPlBase_c *player, bool keep_current_pos) {
     if (keep_current_pos) {
         FakeEntrance.x = player->pos.x - 8.0f;
         FakeEntrance.y = -(player->pos.y + 16.0f);
@@ -74,6 +96,8 @@ void trigger_zone_reload(daPlBase_c *player, bool keep_current_pos) {
     }
 
     player->useNextGotoBlock(FAKE_ENTRANCE_ID, 0, 0);
+
+    is_restoring = true;
 }
 
 
@@ -84,7 +108,7 @@ kmBranchDefAsm(0x8008e3d0, NULL) {
 
     cmpwi r4, FAKE_ENTRANCE_ID
     beq fake
-    lwz r0, 0x90(r3)
+    lwz r0, 0x90(r3)  // original instruction
     b getNextGotoP__9dCdFile_cFUc_PLUS_FOUR
 
 fake:
@@ -107,6 +131,6 @@ kmBranchDefAsm(0x8005ef08, 0x8005ef0c) {
     li r6, 1
 
 finish:
-    clrlwi r4, r3, 28
+    clrlwi r4, r3, 28  // original instruction
     blr
 }
